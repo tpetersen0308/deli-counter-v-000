613912d951a683fb157d8f6572c8bea34d119b91
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-25 19:55:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(names)
+  if names.size == 0
+    puts "The line is currently empty."
+  else
+    names.collect_with_index do |name, index|
+      name.insert(0, " #{index + 1}. ")
+    end
+    puts "The line is currently: #{names.join}"
+  end
+end
+      
